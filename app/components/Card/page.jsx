bf1585545055297031eb93be1a15039a834b842c
yohanesrioirsan <rioirsan8@gmail.com>
2023-10-28 18:03:54 +0700

    fixing Card components, JSON files

--- app/components/Card/page.jsx
+++ app/components/Card/page.jsx
@@ -2,32 +2,37 @@ import "../Card/Card.css";
 import Image from "next/image";
 
 function Card({ weapon }) {
+  if (!weapon || !weapon.weapon) {
+    return null;
+  }
+
   return (
     <div className="card_container">
       <div className="card">
         <div className="weapon_image">
           <Image
             className="weapon_img"
-            src={weapon && weapon.image ? weapon.image : ""}
+            src={weapon.image || ""}
             alt="weapon-image"
             width={350}
             height={300}
           />
         </div>
         <div className="weapon_name">
-          <h1>{weapon.weapon && weapon.weapon.name}</h1>
-          <h1>{weapon.pattern && weapon.pattern.name}</h1>
+          <h1>{weapon.weapon.name}</h1>
+          <h1>{weapon.pattern.name}</h1>
         </div>
         <div className="weapon_collections">
-          {weapon.collections.map((collection) => (
-            <p key={collection.id}>{collection.name}</p>
-          ))}
+          {weapon.collections &&
+            weapon.collections.map((collection) => (
+              <p key={collection.id}>{collection.name}</p>
+            ))}
         </div>
         <div className="hr_line"></div>
         <div className="weapon_details">
           <div className="left_panel">
-            <p>Category: {weapon.category && weapon.category.name}</p>
-            <p>Rarity: {weapon.rarity && weapon.rarity.name}</p>
+            <p>Category: {weapon.category.name}</p>
+            <p>Rarity: {weapon.rarity.name}</p>
             <p>StatTrak Available: {weapon.stattrak ? "Yes" : "No"}</p>
           </div>
           <div className="right_panel">
